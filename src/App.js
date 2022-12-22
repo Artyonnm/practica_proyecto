f1aecb5af7f1e5972b9c4bfc376ebaf9360d01a0
Nacho <ignacio.aguirre@cloud.uautonoma.cl>
2022-12-21 23:43:10 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import foto1 from "./assets/foto1.jpg";
+import foto2 from "./assets/foto2.jpg";
+import foto3 from "./assets/foto3.jpg";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
 function App() {
+  const navbarLinks = [
+    { url: "#", title: "Inicio" },
+    { url: "#", title: "Encargos" },
+    { url: "#", title: "Contacto" },
+    { url: "#", title: "Acerca de" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={foto1} />
+      <Slider
+        imageSrc={foto2}
+        title={"Nuestros productos."}
+        subtitle={
+          "Nuestro servicio ofrece lo que necesitas y a tu alcance."
+        }
+      />
+      <Slider
+        imageSrc={foto3}
+        title={"Estamos comunicados contigo."}
+        subtitle={"Ofrecemos servicio y asesoría para ti cuando quieras."}
+        flipped={true}
+      />
     </div>
   );
 }
